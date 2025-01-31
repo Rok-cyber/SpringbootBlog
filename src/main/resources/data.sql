2943b15b0a2166ff7a1eee2c1e6f03c9188ee932
Rok-cyber <rokbnoc@gmail.com>
2025-01-30 21:59:22 -0600

    SQL UPDATE, added date function.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-Insert INTO article (title, content) VALUES ('Title1', 'Content1')
-Insert INTO article (title, content) VALUES ('Title2', 'Content2')
-Insert INTO article (title, content) VALUES ('Title3', 'Content3')
+Insert INTO article (title, content, created_at, update_ad) VALUES ('Title1', 'Content1', NOW(), NOW())
+Insert INTO article (title, content, created_at, update_ad) VALUES ('Title2', 'Content2', NOW(), NOW())
+Insert INTO article (title, content, created_at, update_ad) VALUES ('Title3', 'Content3', NOW(), NOW())
+
