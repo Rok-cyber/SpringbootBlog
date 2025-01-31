e64218ff9971cd3a0efe15c0af3224c9ec1899b2
Rok-cyber <rokbnoc@gmail.com>
2025-01-30 23:35:13 -0600

    Blog function added

--- src/main/java/seongrok/me/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/seongrok/me/springbootdeveloper/controller/BlogViewController.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import seongrok.me.springbootdeveloper.domain.Article;
 import seongrok.me.springbootdeveloper.dto.ArticleListViewResponse;
+import seongrok.me.springbootdeveloper.dto.ArticleViewResponse;
 import seongrok.me.springbootdeveloper.service.BlogService;
 
 import java.util.List;
@@ -23,6 +27,22 @@ public class BlogViewController {
     model.addAttribute("articles", articles);
 
     return "articleList";
+  }
+  @GetMapping("/articles/{id}")
+  public String getArticle(@PathVariable Long id, Model model) {
+    Article article = blogService.findById(id);
+    model.addAttribute("article", new ArticleViewResponse(article));
+    return "article";
+  }
+  @GetMapping("/new-article/")
+  public String newArticle(@RequestParam(required = false)Long id, Model model){
+    if(id != null) {
+      model.addAttribute("article", new ArticleViewResponse());
+    } else {
+      Article article = blogService.findById(id);
+      model.addAttribute("article", new ArticleViewResponse(article));
+    }
+    return "NewArticle";
   }
 }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-Insert INTO article (title, content, created_at, update_ad) VALUES ('Title1', 'Content1', NOW(), NOW())
-Insert INTO article (title, content, created_at, update_ad) VALUES ('Title2', 'Content2', NOW(), NOW())
-Insert INTO article (title, content, created_at, update_ad) VALUES ('Title3', 'Content3', NOW(), NOW())
+Insert INTO article (title, content, created_at, updated_at) VALUES ('Title1', 'Content1', NOW(), NOW())
+Insert INTO article (title, content, created_at, updated_at) VALUES ('Title2', 'Content2', NOW(), NOW())
+Insert INTO article (title, content, created_at, updated_at) VALUES ('Title3', 'Content3', NOW(), NOW())
 
